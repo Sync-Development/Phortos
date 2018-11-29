fedf03336a2270e000725765d242f5b5f3fe7117
Timothy <31439772+SyncdeveloperT@users.noreply.github.com>
2018-11-29 17:56:06 +0100

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -163,26 +163,23 @@ function filterFunction() {
     <h4> Please fill out this form and we will contact you as soon as possible.</h4>
     <div class="row">
     <div class="col-xs-6"><fieldset>
-    <input placeholder="Uw naam" type="text" tabindex="1" name="name" value="<? $name ?>" autofocus> <span class="error"><?= $name_error ?></span>
+    <input placeholder="First Name" type="text" tabindex="1" name="name" value="<? $name ?>" autofocus> <span class="error"><?= $name_error ?></span>
     </fieldset>
     </div>
     
     <div class="col-xs-6"><fieldset>
-    <input placeholder="Uw E-mail" type="text" name="email" value="<? $email ?>"tabindex="2"> <span class="error"><?= $email_error ?></span>
+    <input placeholder="Last Name" type="text" name="email" value="<? $email ?>"tabindex="2"> <span class="error"><?= $email_error ?></span>
     </fieldset>
     </div>
       </div>
     <fieldset>
-      <input placeholder="Telefoonnummer" type="text" name="phone" value="<? $phone ?>" tabindex="3"> <span class="error"><?= $phone_error ?></span>
+      <input placeholder="E-mail" type="text" name="phone" value="<? $phone ?>" tabindex="3"> <span class="error"><?= $phone_error ?></span>
     </fieldset>
     <fieldset>
-      <input placeholder="Uw website begint met http://" type="text" name="url" value="<? $url ?>" tabindex="4"> <span class="error"><?= $url_error ?></span>
+      <textarea placeholder="Write your message here.." type="text" name="message" value="<? $message ?>" tabindex="5"></textarea>
     </fieldset>
     <fieldset>
-      <textarea placeholder="Schrijf hier uw bericht" type="text" name="message" value="<? $message ?>" tabindex="5"></textarea>
-    </fieldset>
-    <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Bericht verzenden</button>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Send</button>
     </fieldset>
     <div class ="success"><?= $success; ?></div>
   </form>
