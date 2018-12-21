23fc8c730054673d4b5f2fbf090fe448d9b030fa
Joram Baak <31468700+SyncDeveloperJ@users.noreply.github.com>
2018-12-21 11:05:30 +0100

    Hopefully it works now

--- form_process.php
+++ form_process.php
@@ -26,25 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
   
-  if (empty($_POST["phone"])) {
-    $phone_error = "Phone is required";
-  } else {
-    $phone = test_input($_POST["phone"]);
-    // check if e-mail address is well-formed
-    if (!preg_match("/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i",$phone)) {
-      $phone_error = "Invalid phone number"; 
-    }
-  }
 
-  if (empty($_POST["url"])) {
-    $url_error = "";
-  } else {
-    $url = test_input($_POST["url"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)) {
-      $url_error = "Invalid URL"; 
-    }
-  }
 
   if (empty($_POST["message"])) {
     $message = "";
@@ -52,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = test_input($_POST["message"]);
   }
   
-  if ($name_error == '' && $email_error == '' && $phone_error == '' && $url_error == '' ){
+  if ($name_error == '' && $email_error == ''){
       $message_body = '';
       unset($_POST['submit']);
       foreach ($_POST as $key => $value){
